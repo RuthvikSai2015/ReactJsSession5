59a932ad909c2693f9da7b1f86173678e9c842b1
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2024-02-15 19:02:12 +0530

    Children Props

--- myapp/src/ReactRouterExample.js
+++ myapp/src/ReactRouterExample.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, Route, Routes, useNavigate, useParams, Outlet } from 'react-router-dom'
 function Home() {
   return <h1> Home Component </h1>
 }
@@ -9,27 +9,77 @@ function About() {
 function Blog() {
   return <h1> Blog Component</h1>
 }
-function NotFound(){
-  return <h1> Not Found Component </h1>
+function NotFound() {
+  const navigate = useNavigate();
+  return (
+    <>
+      {/* <Link to="/" > Go Back to Home </Link> */}
+      <button onClick={() => navigate('/')} > Go Back to Home </button>
+      <h1> Not Found Component </h1>
+    </>
+  )
 }
-function NavBar(){
-   return (
+function NavBar() {
+  return (
     <nav>
-      <Link to="/"> Home </Link><br/>
-      <Link to="/about"> About </Link><br/>
-      <Link to="/blog"> Blog </Link><br/>
+      <Link to="/"> Home |</Link>
+      <Link to="/about"> About |</Link>
+      <Link to="/blog"> Blog |</Link>
+      <Link to="/posts"> Posts </Link>
     </nav>
-   )
+  )
+}
+function Posts() {
+  const [content, setContent] = useState([]);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then(resp => resp.json())
+      .then(json => setContent(json))
+  }, []);
+  return (
+    <>
+      <Outlet />
+      <ul>
+        {
+          content.map((post) =>
+            <>
+              <li> <Link to={`/posts/${post.id}`}>{post.id}</Link></li>
+              {/* <li> <Link to={`${post.id}`}> {post.id}</Link></li > */}
+            </>
+          )
+        }
+      </ul >
+    </>
+  )
+}
+function Post() {
+  const [content, setContent] = useState([]);
+  const { id } = useParams();
+  console.log(`id in post component ----${id}`);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(resp => resp.json())
+      .then(json => setContent(json))
+  }, [id]);
+  return (
+    <>
+      <p>{content.id}</p>
+      <p>{content.body}</p>
+    </>
+  )
 }
 export default function ReactRouterExample() {
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/about" element = {<About/>}/>
-        <Route path="/blog" element = {<Blog/>}/>
-        <Route path="*" element = {<NotFound/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/posts" element={<Posts />} >
+          <Route path=":id" element={<Post />} />
+        </Route>
       </Routes>
 
     </div>
